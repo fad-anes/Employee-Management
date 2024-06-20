c3313be339937d2e3721003b2789c907ba51c4fb
fad-anes <anes.fad@esprit.tn>
2024-06-21 00:44:40 +0100

    creation du fichier controlleur avec les functions crud

---
+++ src/Controlleur/employeeController.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import Employee from '../Model/Employee';
+
+// Ajouter un nouvel employé
+export const addEmployee = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const employee = new Employee(req.body);
+      await employee.save();
+      res.status(201).json(employee);
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+  };
+
+  // Mettre à jour un employé existant
+export const updateEmployee = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const employee = await Employee.findByIdAndUpdate(id, req.body, { new: true });
+      if (!employee) {
+        res.status(404).json({ message: 'Employee not found' });
+        return;
+      }
+      res.status(200).json(employee);
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+  };
+  
+  // Supprimer un employé par ID
+  export const deleteEmployee = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const employee = await Employee.findByIdAndDelete(id);
+      if (!employee) {
+        res.status(404).json({ message: 'Employee not found' });
+        return;
+      }
+      res.status(200).json({ message: 'Employee deleted' });
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+  };
--- src/Model/Employee.ts
+++ src/Model/Employee.ts
@@ -1,25 +1,20 @@
-import mongoose from 'mongoose';
-const Schema = mongoose.Schema;
-const Employee = new Schema({
-    name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-        unique: true,
-      },
-      position: {
-        type: String,
-        required: true,
-      },
-      department: {
-        type: String,
-      },
-      salary: {
-        type: Number,
-      },
-    });
+import mongoose, { Document, Schema } from 'mongoose';
+// L'interface étend Document de Mongoose pour inclure les propriétés d'un document MongoDB
+interface IEmployee extends Document {
+  name: string;
+  email: string;
+  position: string;
+  department?: string;
+  salary?: number;
+}
+// Le schéma définit la structure des documents dans la collection MongoDB
+const EmployeeSchema: Schema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  position: { type: String, required: true },
+  department: { type: String },
+  salary: { type: Number },
+});
 
-module.exports = mongoose.model('Employee', Employee);
+const Employee = mongoose.model<IEmployee>('Employee', EmployeeSchema);
+export default Employee;
