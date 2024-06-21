58827ca22bd180eb4b083174e219825fe4d344c5
fad-anes <anes.fad@esprit.tn>
2024-06-21 02:30:37 +0100

    middleware , readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Employee-Management
 A simple Rest API using node.js ,express(using TypeScript) and MongoDb to manage employee records
+## Installation 
+Use the package manager [npm](https://www.npmjs.com/) to install dependencies.
+```bash
+npm install 
+```
+## config
+add the .env file(provided in the email) to the project (outside the src folder)
+## lanch
+to start the app you just need to 
+```bash
+npm run dev 
+```
+## Endpoints
+POST "http://localhost:3000/employees" to add an employee 
+exemple {"name":"employeeOne",
+    "email":"employee@exemple.com",
+    "position":"positionOne",
+    "department":"departmentThree",
+    "salary":1500}
+    RQ : Email must be unique and have the correct format. Name and position are required and must be strings. Department and salary are optional, but if specified, salary must be numeric. Otherwise, an appropriate status and message should be generated for errors. 
+PUT "http://localhost:3000/employees/:id" to modify an existing employee
+DELETE "http://localhost:3000/employees/:id" to delete an employee 
--- src/Route/employeeRoutes.ts
+++ src/Route/employeeRoutes.ts
@@ -1,20 +1,19 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import { addEmployee, updateEmployee, deleteEmployee } from '../Controlleur/employeeController';
-import { Request, Response } from 'express';
-
+import { validateEmployee, validateEmployeeUpdate,validateEmployeeDelete } from '../middleware/validationMiddleware';
 const router = Router();
 
 // POST /employees - Ajouter un nouvel employé
-router.post('', async (req: Request, res: Response) => {
+router.post('',validateEmployee, async (req: Request, res: Response) => {
       await addEmployee(req, res);
   });
 
 // PUT /employees/:id - Mettre à jour un employé existant
-router.put('/:id', async (req: Request, res: Response) => {
+router.put('/:id',validateEmployeeUpdate, async (req: Request, res: Response) => {
     await updateEmployee(req, res);
 });
 // DELETE /employees/:id - Supprimer un employé par ID
-router.delete('/:id', async (req: Request, res: Response) => {
+router.delete('/:id',validateEmployeeDelete, async (req: Request, res: Response) => {
     await deleteEmployee(req, res);
 });
 export default router;
